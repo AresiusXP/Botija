ae1039ad2d9623c0e2a16b29fe966af4f2695fe0
Patricio Danos <aresiusxp@gmail.com>
2020-07-29 20:24:56 -0300

    Added time calculation for reminder

--- botija.py
+++ botija.py
@@ -1,5 +1,7 @@
 import os
 import discord
+from datetime import datetime,timedelta 
+from dateutil.relativedelta import relativedelta
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -27,5 +29,21 @@ async def on_ready():
 @bot.command(name="hello", help="It says hello back!")
 async def hello_chat(ctx):
     await ctx.send(f'Hello {ctx.message.author.name}!')
+
+@bot.command(name="RemindMe", help="Creates a reminder. Syntax: !RemindMe [int] [m|h|d|M|y] \"Message to record\"")
+async def remind_me(ctx, amount: int, time, message)
+    current_time=datetime.now()
+    if time == "m":
+        reminder_time=current_time + timedelta(minutes=amount)
+    if time == "h":
+        reminder_time=current_time + timedelta(hours=amount)
+    if time == "d":
+        reminder_time=current_time + timedelta(days=amount)
+    if time == "M":
+        reminder_time=current_time + relativedelta(months=+amount)
+    if time == "y":
+        reminder_time=current_time + relativedelta(years=+amount)
+
+    await ctx.send("Your reminders has been set for {reminder_time} with message \"{message}\"")
 
 bot.run(TOKEN)
