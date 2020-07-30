4cc352844e5a99c0ce55ab0fb5b20367d4982190
Patricio Danos <aresiusxp@gmail.com>
2020-07-30 00:12:01 -0300

    Changed quotes connection string

--- sql.py
+++ sql.py
@@ -3,11 +3,13 @@ import pyodbc
 from dotenv import load_dotenv
 
 # Gather parameters
+load_dotenv()
 SQL_SERVER=os.getenv('SQL_SERVER')
 SQL_DB=os.getenv('SQL_DB')
 SQL_USER=os.getenv('SQL_USER')
 SQL_PASS=os.getenv('SQL_PASS')
 
 # Connect to SQL Database
-sql_client = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER="+SQL_SERVER+";DATABASE="+SQL_DB+";UID="+SQL_USER+";PWD="+SQL_PASS)
+sql_client = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+SQL_SERVER+";DATABASE="+SQL_DB+";UID="+SQL_USER+";PWD="+SQL_PASS)
 cursor = sql_client.cursor()
+
