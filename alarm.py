1d5020ac6fc0b845e00f2846a573fb0ac5db1522
Patricio Danos <aresiusxp@gmail.com>
2020-07-30 21:12:44 -0300

    Specified data types, added author_name

--- alarm.py
+++ alarm.py
@@ -1,8 +1,11 @@
+import datetime
+
 class alarm:
-    def __init__(self, reminder_time, message, channel, channel_id, guild_name, author_id):
+    def __init__(self, reminder_time: datetime, message, channel, channel_id: int, guild_name, author_id: int, author_name):
         self.reminder_time = reminder_time
         self.message = message
         self.channel = channel
         self.channel_id = channel_id
         self.guild_name = guild_name
         self.author_id = author_id
+        self.author_name = author_name
